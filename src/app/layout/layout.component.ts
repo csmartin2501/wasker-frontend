16315d68c7f516fa1a5700edf26ce90f0de57f92
Camilo Valenzuela <cvalenzuela@plataformagroup.cl>
2026-02-26 21:56:29 -0300

    layout css

--- src/app/layout/layout.component.ts
+++ src/app/layout/layout.component.ts
@@ -2,7 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-layout',
-  templateUrl: './layout.component.html'
+  templateUrl: './layout.component.html',
+  styleUrls: ['./layout.component.css']
 })
 export class LayoutComponent {
   currentYear = new Date().getFullYear();
